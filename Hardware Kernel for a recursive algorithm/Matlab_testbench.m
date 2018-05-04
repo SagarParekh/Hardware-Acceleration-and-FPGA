ee94e1ab4f6a17fc8331816b9261e96583431cc2
Sagar Parekh <sagarparekh.sjp@gmail.com>
2018-05-04 16:39:25 -0700

    Add files via upload

---
+++ Hardware Kernel for a recursive algorithm/Matlab_testbench.m
@@ -0,0 +1,26 @@
+%% Generate test vectors
+
+
+input_sample_rate = 31.56e6; % Hz 
+x=2;
+
+t = [0:63]'/input_sample_rate;
+xt.time = 0;
+xt.signals.values = x;
+xt.signals.dimensions = 1;
+
+%% Configure Simulink models
+% %List all the % parameters of a block
+% load_system('CEN598_HW2/From Workspace');
+% list = get_param('CEN598_HW2/From Workspace','ObjectParameters');
+
+% Set the value of a parameter in a block
+BlockName = 'HW3_Q5';
+
+set_param([BlockName '/To Workspace'],'VariableName','yt1')
+set_param([BlockName '/To Workspace'],'SaveFormat',['Structure With Time'])
+
+set_param([BlockName '/From Workspace'],'VariableName','xt')
+
+%% Verify the results 
+
